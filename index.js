e9e9f093bfa1741f71a6af54fa61170606233b84
Scott Schwab <scottschwab86@gmail.com>
2023-05-08 17:44:42 -0500

    Add ability to retrieve all pieces and append them to display

--- index.js
+++ index.js
@@ -6,38 +6,68 @@ const initialNav = document.querySelector('#initial-nav');
 const addPieceFormArea = document.querySelector('#add-piece-form-area');
 const retrieveForm = document.querySelector('#retrieve-form');
 const addPieceForm = document.querySelector('#add-piece-form');
+const listAllBtn = document.querySelector('#main-retrieve-all-btn');
+const repListing = document.querySelector('#rep-listing');
 let pieces;
 
 // FETCH DATA FROM DB AND PUT INTO GLOBAL VARIABLE
-fetch('http://localhost:3000/pieces')
-  .then(res => res.json())
-  .then(data => handleData(data));
+const retrievePiecesFromDb = () => {
+  fetch('http://localhost:3000/pieces')
+    .then(res => res.json())
+    .then(data => handleData(data));
 
-const handleData = (data) => {
-  pieces = data;
-  console.log('here are your pieces', pieces)
+  const handleData = (data) => {
+    pieces = data;
+    console.log('here are your pieces', pieces)
+  }
+  return pieces; // I'm not crazy about this global variable, will consult with teacher about if this is the best way to do this
 }
+retrievePiecesFromDb();
+
 
 // INITIAL LISTENERS
 mainAddBtn.addEventListener('click', () => {
   initialNav.style.display = 'none';
   addPieceFormArea.style.display = 'flex';
-
-})
+});
 
 mainRetrieveBtn.addEventListener('click', () => {
   initialNav.style.display = 'none';
   retrieveForm.style.display = 'flex';
+});
 
+listAllBtn.addEventListener('click', () => {
+  let allPieces = retrievePiecesFromDb();
+  allPieces.forEach(piece => {
+    const pieceCard = document.createElement('div');
+
+    const pieceTitle = document.createElement('p');
+    pieceTitle.textContent = piece.title;
+
+    const pieceComposer = document.createElement('p');
+    pieceComposer.textContent = piece.composer;
+
+    const pieceInstrument = document.createElement('p');
+    pieceInstrument.textContent = piece.instrument;
+
+    pieceCard.classList.add('piece-card');
+    pieceCard.append(pieceTitle);
+    pieceCard.append(pieceComposer);
+    pieceCard.append(pieceInstrument);
+
+    repListing.append(pieceCard);
+  })
 })
 
+
+
+// HANDLE SUBMISSION OF NEW PIECE
 addPieceForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('submitted');
   const titleField = document.querySelector('#piece-title');
   const composerField = document.querySelector('#composer');
   const instrumentSelection = document.querySelector('#instrument');
-  // console.log(titleField.value, composerField.value, instrumentSelection.value)
+
   fetch('http://localhost:3000/pieces', {
     method: 'POST',
     headers: {
@@ -50,8 +80,12 @@ addPieceForm.addEventListener('submit', (e) => {
       instrument: instrumentSelection.value
     })
   })
-  .then(res => res.json())
-  .then(data => console.log(data))  // here we can give a success message and reset the input fields
-
+    .then(res => res.json())
+    .then(data => console.log(data))  // here we can give a success message and reset the input fields
 });
+
+retrieveForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(e)
+})
 
