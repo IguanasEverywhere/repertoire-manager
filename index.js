e22f78bc70a6afb1adaa48c32fe6ecb657917076
Scott Schwab <scottschwab86@gmail.com>
2023-05-08 11:22:00 -0500

    Add HTML forms and firstNav listeners

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('test')
+// ELEMENT SELECTIONS
+const navigationArea = document.querySelector('#navigation-area');
+const mainAddBtn = document.querySelector('#main-add-btn');
+const mainRetrieveBtn = document.querySelector('#main-retrieve-btn');
+const initialNav = document.querySelector('#initial-nav');
+const addPieceForm = document.querySelector('#add-piece-form');
+const retrieveForm = document.querySelector('#retrieve-form');
+
+// INITIAL LISTENERS
+mainAddBtn.addEventListener('click', () => {
+  initialNav.style.display = 'none';
+  addPieceForm.style.display='flex';
+
+})
+
+mainRetrieveBtn.addEventListener('click', () => {
+  initialNav.style.display = 'none';
+  retrieveForm.style.display = 'flex';
+
+})
